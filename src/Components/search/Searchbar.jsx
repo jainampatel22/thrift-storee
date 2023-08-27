e3624af083d6ed3a0aeba9eaf301f09be247662b
Jainam <pateljainams122@gmail.com>
2023-08-27 06:48:49 -0700

    Update Searchbar.jsx

--- src/Components/search/Searchbar.jsx
+++ src/Components/search/Searchbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState , useEffect } from 'react'
 import './Searchbar.scss'
-import './SearchResult'
+
 import axios from 'axios'
 import {AiOutlineCloseCircle} from "react-icons/ai"
 import { useNavigate } from 'react-router-dom'
