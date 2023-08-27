e1bd27c8650e897494f8608c03e1816979ffb780
Jainam <pateljainams122@gmail.com>
2023-08-26 22:40:43 -0700

    Update HeaderDetails.jsx

--- src/Components/Header/HeaderDetails.jsx
+++ src/Components/Header/HeaderDetails.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import banner from './banner-img.png'
-import {toast} from 'react-toastify';
-toast.configure()
+
 function HeaderDetails() {
-const notify=()=>{
-  toast('Product Is Out Of Stock!')
-}
+
 
   return (
     <>
@@ -23,7 +20,7 @@ const notify=()=>{
      <span className="desc">
      Elevate your audio experience with our premium headphones on sale. Designed to perfection, they offer high-definition sound and a comfortable fit for extended wear. Seamlessly connect via Bluetooth and enjoy the freedom to move without tangling wires. Intuitive touch controls on the earcups provide easy navigation, while the long-lasting battery ensures hours of playback. Don't miss this chance to own top-notch headphones at an incredible price. Upgrade your listening journey today!
  </span>
-  <button className='buy-btn' onClick={notify}>add to cart</button> 
+  <button className='buy-btn' >add to cart</button> 
            
          </div>
        </div>
