d3cc0e3aa04ceea5bc9547d28561b9dbcfcfc3e6
Jainam <pateljainams122@gmail.com>
2023-08-26 22:32:35 -0700

    Update HeaderDetails.jsx

--- src/Components/Header/HeaderDetails.jsx
+++ src/Components/Header/HeaderDetails.jsx
@@ -17,7 +17,7 @@ function HeaderDetails() {
      <span className="desc">
      Elevate your audio experience with our premium headphones on sale. Designed to perfection, they offer high-definition sound and a comfortable fit for extended wear. Seamlessly connect via Bluetooth and enjoy the freedom to move without tangling wires. Intuitive touch controls on the earcups provide easy navigation, while the long-lasting battery ensures hours of playback. Don't miss this chance to own top-notch headphones at an incredible price. Upgrade your listening journey today!
  </span>
-  <button  >add to cart</button> 
+  <button className='buy-btn' >add to cart</button> 
            
          </div>
        </div>
